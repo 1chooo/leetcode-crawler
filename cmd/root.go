7f2921d28b43c89c3a9e2bec0e3975fcb896d86a
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-31 17:34:15 +0800

    feat: implement core functionality flag for leetcode-crawler CLI

---
+++ cmd/root.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "leetcode-crawler",
+	Short: "leetcode-crawler is a cli tool for crawling leetcode problems.",
+	Long:  "leetcode-crawler is a command line tool that allows you to crawl and manage leetcode problems efficiently.",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+var (
+	version = "0.0.1"
+	author  = "Chun-Ho (Hugo) Lin"
+)
+
+func Execute() {
+	rootCmd.PersistentFlags().BoolP("version", "v", false, "Print the version information and quit")
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		showVersion, _ := cmd.Flags().GetBool("version")
+		if showVersion {
+			fmt.Printf("Version: %s\nAuthor: %s\n", version, author)
+			os.Exit(0)
+		}
+	}
+
+    rootCmd.PersistentFlags().StringP("problem", "p", "", "crawl leetcode problems by problem number or tag, e.g., 1, 1-10, array, string")
+    rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+        problem, _ := cmd.Flags().GetString("problem")
+        if problem == "" {
+            fmt.Fprintln(os.Stderr, "Error: You must specify a problem number or tag using the --problem flag.")
+            os.Exit(1)
+        }
+        fmt.Printf("Crawling problems for: %s\n", problem)
+    }
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Oops. An error while crawling leetcode problems: \n%s\n", err)
+		os.Exit(1)
+	}
+}
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"github.com/1chooo/leetcode-crawler/cmd"
+)
 
 func main() {
-	fmt.Println("Hello, LeetCode Crawler!")
+    cmd.Execute()
 }
