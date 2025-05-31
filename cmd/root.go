96bba22b3cb15e0cd37125b29897b4d69e8df930
Hugo ChunHo Lin <hugo970217@gmail.com>
2025-05-31 20:07:03 +0800

    feat: show help if no subcommands or flags are probided and define international flag

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ var rootCmd = &cobra.Command{
 	Short: "leetcode-crawler is a cli tool for crawling leetcode problems.",
 	Long:  "leetcode-crawler is a command line tool that allows you to crawl and manage leetcode problems efficiently.",
 	Run: func(cmd *cobra.Command, args []string) {
-
+		cmd.Help()
 	},
 }
 
@@ -30,15 +30,27 @@ func Execute() {
 		}
 	}
 
-    rootCmd.PersistentFlags().StringP("problem", "p", "", "crawl leetcode problems by problem number or tag, e.g., 1, 1-10, array, string")
-    rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
-        problem, _ := cmd.Flags().GetString("problem")
-        if problem == "" {
-            fmt.Fprintln(os.Stderr, "Error: You must specify a problem number or tag using the --problem flag.")
-            os.Exit(1)
-        }
-        fmt.Printf("Crawling problems for: %s\n", problem)
-    }
+	rootCmd.PersistentFlags().StringP("problem", "p", "", "crawl leetcode problems by problem number or tag, e.g., 1, 1-10, array, string")
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		problem, _ := cmd.Flags().GetString("problem")
+		if problem == "" {
+			fmt.Fprintln(os.Stderr, "Error: You must specify a problem number or tag using the --problem flag.")
+			os.Exit(1)
+		}
+		// fmt.Printf("Crawling problems for: %s\n", problem)
+	}
+
+	rootCmd.PersistentFlags().StringP("international", "i", "", "Specify the international version of leetcode, options: en, cn")
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		international, _ := cmd.Flags().GetString("international")
+		if international == "" {
+			international = "en" // Default to English if not specified
+		}
+		if international != "en" && international != "cn" {
+			fmt.Fprintln(os.Stderr, "Error: Invalid international option. Use 'en' for English or 'cn' for Chinese.")
+			os.Exit(1)
+		}
+	}
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintf(os.Stderr, "Oops. An error while crawling leetcode problems: \n%s\n", err)
